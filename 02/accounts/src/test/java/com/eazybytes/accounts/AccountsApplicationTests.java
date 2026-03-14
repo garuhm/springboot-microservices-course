4b337d54680d1600ace1602e84c62acd61ccfb10
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-03-14 13:31:04 -0400

    02 refactor

--- 02/accounts/src/test/java/com/eazybytes/accounts/AccountsApplicationTests.java
+++ 02/accounts/src/test/java/com/eazybytes/accounts/AccountsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microservices.accounts;
+package com.eazybytes.accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AccountsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
